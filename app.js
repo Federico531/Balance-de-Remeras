7bd130fe8c2182bb32cf8490225591ee4b4aa32c
Federico_531 <federicomarchetta@yahoo.com.ar>
2020-04-30 19:53:04 -0300

    Pude reducir getElementById en un if, pero me dejo de funcionar bien sort

--- app.js
+++ app.js
@@ -37,85 +37,35 @@ const addDesign = (ev)=>{
      
              
 //RANKING DE VENTAS
+var index = 0;
 
 var balanceVenta = (ev) =>{
     ev.preventDefault();  
 
-    diseños.sort((a,b) => { return (a.ammountSold < b.ammountSold) ? 1 : -1 });   
+    index ++ ;
+    var prefixName= "name";
+    var prefixStock= "stock";
+    var prefixPrice = "price";
+    var prefixSold = "sold";
+    var prefixLastProd = "lastProduction";
+    var earnings = "earnings";
 
-    document.getElementById("name1").innerHTML = diseños[0].designName;
-    document.getElementById("stock1").innerHTML = diseños[0].currentStock;
-    document.getElementById("price1").innerHTML =  "$" + diseños[0].priceEa;
-    document.getElementById("sold1").innerHTML = diseños[0].ammountSold;
-    document.getElementById("lastProduction1").innerHTML = diseños[0].productionAmmount;
-//  document.getElementById("earnings1").innerHTML = resultado de price x sold
+    if(diseños.length == index ){
+        document.getElementById(prefixName + index).innerHTML = diseños[index - 1].designName;
+        document.getElementById(prefixStock + index).innerHTML = diseños[index - 1].currentStock;
+        document.getElementById(prefixPrice + index).innerHTML = diseños[index - 1].priceEa;
+        document.getElementById(prefixSold+ index).innerHTML = diseños[index - 1].ammountSold;
+        document.getElementById(prefixLastProd + index).innerHTML = diseños[index - 1].productionAmmount;
+    }
+}
 
-    document.getElementById("name2").innerHTML = diseños[1].designName;
-    document.getElementById("stock2").innerHTML = diseños[1].currentStock;
-    document.getElementById("price2").innerHTML = "$" +  diseños[1].priceEa;
-    document.getElementById("sold2").innerHTML = diseños[1].ammountSold;
-    document.getElementById("lastProduction2").innerHTML = diseños[1].productionAmmount;
-//  document.getElementById("earnings1").innerHTML = resultado de price x sold
+var sort = (ev)=>{
+    ev.preventDefault();  
 
-    document.getElementById("name3").innerHTML = diseños[2].designName;
-    document.getElementById("stock3").innerHTML = diseños[2].currentStock;
-    document.getElementById("price3").innerHTML =  "$" + diseños[2].priceEa;
-    document.getElementById("sold3").innerHTML = diseños[2].ammountSold;
-    document.getElementById("lastProduction3").innerHTML = diseños[2].productionAmmount;
-//  document.getElementById("earnings1").innerHTML = resultado de price x sold
-
-    document.getElementById("name4").innerHTML = diseños[3].designName;
-    document.getElementById("stock4").innerHTML = diseños[3].currentStock;
-    document.getElementById("price4").innerHTML = "$" + diseños[3].priceEa;
-    document.getElementById("sold4").innerHTML = diseños[3].ammountSold;
-    document.getElementById("lastProduction4").innerHTML = diseños[3].productionAmmount;
-//  document.getElementById("earnings1").innerHTML = resultado de price x sold
-
-    document.getElementById("name5").innerHTML = diseños[4].designName;
-    document.getElementById("stock5").innerHTML = diseños[4].currentStock;
-    document.getElementById("price5").innerHTML = "$" + diseños[4].priceEa;
-    document.getElementById("sold5").innerHTML = diseños[4].ammountSold;
-    document.getElementById("lastProduction5").innerHTML = diseños[4].productionAmmount;
-//  document.getElementById("earnings1").innerHTML = resultado de price x sold
-
-    document.getElementById("name6").innerHTML = diseños[5].designName;
-    document.getElementById("stock6").innerHTML = diseños[5].currentStock;
-    document.getElementById("price6").innerHTML = "$" + diseños[5].priceEa;
-    document.getElementById("sold6").innerHTML = diseños[5].ammountSold;
-    document.getElementById("lastProduction6").innerHTML = diseños[5].productionAmmount;
-//  document.getElementById("earnings1").innerHTML = resultado de price x sold
-
-    document.getElementById("name7").innerHTML = diseños[6].designName;
-    document.getElementById("stock7").innerHTML = diseños[6].currentStock;
-    document.getElementById("price7").innerHTML = "$" + diseños[6].priceEa;
-    document.getElementById("sold7").innerHTML = diseños[6].ammountSold;
-    document.getElementById("lastProduction7").innerHTML = diseños[6].productionAmmount;
-//  document.getElementById("earnings1").innerHTML = resultado de price x sold
-
-    document.getElementById("name8").innerHTML = diseños[7].designName;
-    document.getElementById("stock8").innerHTML = diseños[7].currentStock;
-    document.getElementById("price8").innerHTML = "$" + diseños[7].priceEa;
-    document.getElementById("sold8").innerHTML = diseños[7].ammountSold;
-    document.getElementById("lastProduction8").innerHTML = diseños[7].productionAmmount;
-//  document.getElementById("earnings1").innerHTML = resultado de price x sold
-
-    document.getElementById("name9").innerHTML = diseños[8].designName;
-    document.getElementById("stock9").innerHTML = diseños[8].currentStock;
-    document.getElementById("price9").innerHTML = "$" + diseños[8].priceEa;
-    document.getElementById("sold9").innerHTML = diseños[8].ammountSold;
-    document.getElementById("lastProduction9").innerHTML = diseños[8].productionAmmount;
-//  document.getElementById("earnings1").innerHTML = resultado de price x sold
-
-    document.getElementById("name10").innerHTML = diseños[9].designName;
-    document.getElementById("stock10").innerHTML = diseños[9].currentStock;
-    document.getElementById("price10").innerHTML = "$" + diseños[9].priceEa;
-    document.getElementById("sold10").innerHTML = diseños[9].ammountSold;
-    document.getElementById("lastProduction10").innerHTML = diseños[9].productionAmmount;
-//  document.getElementById("earnings1").innerHTML = resultado de price x sold
+    diseños.sort((a,b) => { return (a.ammountSold > b.ammountSold) ? 1 : -1 });    
 
 }
 
-//Con esto estoy agarrando dos elementos del indice 0 y concatenandolos (BOCETO PROVISORIO)
 const display = (ev)=>{
     ev.preventDefault();  
 
@@ -138,5 +88,6 @@ const display = (ev)=>{
     document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('agregarDiseño').addEventListener('click',addDesign);
     document.getElementById('agregarDiseño').addEventListener('click',balanceVenta);
+    document.getElementById('sort').addEventListener('click',sort);
     document.getElementById('display').addEventListener('click', display);
 }); 
