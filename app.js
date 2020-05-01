e24291d06f7f408f5de8c30981976364ad1b63dd
Federico_531 <federicomarchetta@yahoo.com.ar>
2020-05-01 14:26:49 -0300

    Version 1.1 Funcionando

--- app.js
+++ app.js
@@ -34,37 +34,28 @@ const addDesign = (ev)=>{
     //saving to localStorage
     localStorage.setItem('Mydesignlist', JSON.stringify(diseños) );
     };
-     
-             
-//RANKING DE VENTAS
-var index = 0;
-
-var balanceVenta = (ev) =>{
-    ev.preventDefault();  
-
-    index ++ ;
-    var prefixName= "name";
-    var prefixStock= "stock";
-    var prefixPrice = "price";
-    var prefixSold = "sold";
-    var prefixLastProd = "lastProduction";
-    var earnings = "earnings";
-
-    if(diseños.length == index ){
-        document.getElementById(prefixName + index).innerHTML = diseños[index - 1].designName;
-        document.getElementById(prefixStock + index).innerHTML = diseños[index - 1].currentStock;
-        document.getElementById(prefixPrice + index).innerHTML = diseños[index - 1].priceEa;
-        document.getElementById(prefixSold+ index).innerHTML = diseños[index - 1].ammountSold;
-        document.getElementById(prefixLastProd + index).innerHTML = diseños[index - 1].productionAmmount;
-    }
-}
-
-var sort = (ev)=>{
-    ev.preventDefault();  
-
-    diseños.sort((a,b) => { return (a.ammountSold > b.ammountSold) ? 1 : -1 });    
-
-}
+  
+    var balanceVenta = (ev) => {
+        ev.preventDefault();
+      
+        diseños.sort((a, b) => b.ammountSold - a.ammountSold);
+      
+        var prefixName = "name";
+        var prefixStock = "stock";
+        var prefixPrice = "price";
+        var prefixSold = "sold";
+        var prefixLastProd = "lastProduction";
+      
+        for (var index = 0; index < diseños.length; index++) {
+          var suffix = index + 1;
+          document.getElementById(prefixName + suffix).innerHTML = diseños[index].designName;
+          document.getElementById(prefixStock + suffix).innerHTML = diseños[index].currentStock;
+          document.getElementById(prefixPrice + suffix).innerHTML = "$" + diseños[index].priceEa;
+          document.getElementById(prefixSold + suffix).innerHTML = diseños[index].ammountSold;
+          document.getElementById(prefixLastProd + suffix).innerHTML = diseños[index].productionAmmount;
+        }
+      
+      }
 
 const display = (ev)=>{
     ev.preventDefault();  
@@ -74,20 +65,14 @@ const display = (ev)=>{
         var earned = diseños[0].priceEa * diseños[0].ammountSold;
         var stockSum= a + b;
 
-        var resultadoProvisorio = "La remera con el Diseño: " + diseños[0].designName + " tenia en stock " + diseños[0].productionAmmount +
-         " remeras antes de comenzar, pero luego de esta produccion seran " +  stockSum + "." + "<br>" +  "Si se vendieron " + diseños[0].ammountSold + 
-         " ganaré $" + earned;
 
          //Que aparezca un boton con el nombre del diseño
         var obj1Name = diseños[0].designName;
      
         document.getElementById("buttonAppear1").innerHTML = "<button onclick=\"secondFunction()\"></button>";
         document.getElementById("buttonAppear1").innerHTML = obj1Name;
-        document.getElementById("demo1").innerHTML = resultadoProvisorio;   
 }
     document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('agregarDiseño').addEventListener('click',addDesign);
     document.getElementById('agregarDiseño').addEventListener('click',balanceVenta);
-    document.getElementById('sort').addEventListener('click',sort);
-    document.getElementById('display').addEventListener('click', display);
 }); 
